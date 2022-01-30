c89f97f92c2739652fd050af93b7d319b2914c68
Rian <rbappdevelopment@gmail.com>
2022-01-30 21:11:09 +0100

    Adjusted new db migration file for fix

--- database/migrations/2022_01_30_999999_CreateHousesAndParticipantsTables.php
+++ database/migrations/2022_01_30_999999_CreateHousesAndParticipantsTables.php
@@ -13,35 +13,39 @@ class CreateHousesAndParticipantsTables extends Migration
      */
     public function up()
     {
-        Schema::table('house', function (Blueprint $table) {
-            $table->primary('id');
+        Schema::create('house', function (Blueprint $table) {
+            $table->bigIncrements('id');
         });
 
-        Schema::table('houseInfo', function (Blueprint $table) {
-            $table->id();
+        Schema::create('houseInfo', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('house_id')->unsigned();
             $table->foreign('house_id')->references('id')->on('house'); //foreign key constraint
             $table->string('name');
             $table->string('created_by_uid');
             $table->date('created_date');
-        });
+        });  
 
-        Schema::table('houseParticipantMap', function (Blueprint $table) {
-            $table->foreign('house_id')->references('id')->on('house'); //foreign key constraint
-            $table->foreign('participant_id')->references('id')->on('participant'); //foreign key constraint
-            $table->integer('drinks_count');
-            $table->integer('is_admin')->default('0');
-        });    
-
-        Schema::table('participant', function (Blueprint $table) {
-            $table->id();
+        Schema::create('participant', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('house_id')->unsigned();
             $table->foreign('house_id')->references('id')->on('house'); //foreign key constraint
             $table->string('display_name');
             $table->date('created_date');
             $table->foreignId('user_id')->nullable()->constrained('users'); //if not null -> user has registered an account and can be found in the users table
         });
+
+        Schema::create('houseParticipantMap', function (Blueprint $table) {
+            $table->unsignedBigInteger('house_id')->unsigned();
+            $table->foreign('house_id')->references('id')->on('house'); //foreign key constraint
+            $table->unsignedBigInteger('participant_id')->unsigned();
+            $table->foreign('participant_id')->references('id')->on('participant'); //foreign key constraint
+            $table->integer('drinks_count');
+            $table->integer('is_admin')->default('0');
+        });  
         
         Schema::create('mutaties', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->integer('pid');
             $table->integer('amount');
             $table->integer('total_left');
